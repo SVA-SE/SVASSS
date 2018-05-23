7a9dbdc3fcef5ec8d4e795b632382b765b54b794
nandadorea <dorea.meyer@gmail.com>
2018-05-23 11:02:30 +0200

    not run if no new data

--- FOD.r
+++ FOD.r
@@ -23,5 +23,7 @@ CD.species$SYNDROMIC[(!CD.species$General=="Mycotic")&(!CD.species$General=="Bac
 #   2. run detection algorithms;
 #   3. report alarms)
 
-source("vetsyn-based-engine.r",local=TRUE,encoding="native.enc")
+if(dim(CD.species)[1]>0){
+
+source("vetsyn-based-engine.r",local=TRUE,encoding="native.enc")}
 
