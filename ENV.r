cb9615c926a32799796b961ef25b1528fa982b56
nandadorea <dorea.meyer@gmail.com>
2018-08-14 08:40:42 +0200

    ENV, FSK and FOD were not doing svaga "by syndromes", but for all ENV/FSK/FOD. The problem with that, is the way the structures of data are expected, in particular to plot things in Shiny
    reverted back to syndrome specific summaries

--- ENV.r
+++ ENV.r
@@ -1,4 +1,4 @@
-sp.label="Environment"
+sp.label="Environmental"
 sp.acron="ENV"
 sp.position=which(species.acronyms==sp.acron)
 
@@ -11,7 +11,7 @@ sp.position=which(species.acronyms==sp.acron)
   #different from every otehr "species"
   #because its not in the species column, but syndromic
 CD.species <-current.SVA.data[current.SVA.data$SYNDROMIC==sp.label,] 
-
+CD.species$SPECIES <- "Environmental"
 
 CD.species$SYNDROMIC[CD.species$Specific=="E.coli"] <- "E.coli"
 CD.species$SYNDROMIC[CD.species$Specific=="Salmonella"] <- "Salmonella"
@@ -35,55 +35,62 @@ source("vetsyn-based-engine.r",local=TRUE,encoding="native.enc")
 
 # svaga.data.filtering ----
 
-#for (s in c(sp.colnames[[sp.position]],"Nonspecific")){
-  
-  #CD.syndrome <- CD.species[CD.species$SYNDROMIC==s,c("AGENS","PÅVISAD","INSÄNTMATERIAL","ANKOMSTDATUM")]
-  CD.syndrome <- CD.species[,c("AGENS","PÅVISAD","INSÄNTMATERIAL","ANKOMSTDATUM")]
-  
-#non.svaga.object[[sp.position]][[s]]<-CD.species[CD.species$AGENS=="",]
-  
-  
-  CD.syndrome<-CD.syndrome[CD.syndrome$AGENS!="",]
-  
-  if(dim(CD.syndrome)[1]>0){
-    
-    svaga.object[[sp.position]][[s]] <- svaga.object[[sp.position]][[s]][1:last.historical.row.week,,,drop=FALSE]
-    
-    CD.syndrome$AGENS<- str_replace(CD.syndrome$AGENS, "\\(\\+\\)", "")
-    syndromes.name=unique(c(colnames(svaga.object[[sp.position]][[s]]),CD.syndrome$AGENS))
-    
-    a.all=raw_to_syndromicW(id=INSÄNTMATERIAL,
-                            syndromes.var=AGENS,
-                            syndromes.name=syndromes.name,
-                            dates.var=ANKOMSTDATUM,
-                            date.format = "%d/%m/%Y",
-                            min.date=new.data.start,
-                            max.date=new.data.end,
-                            data=CD.syndrome)
-    
-    CD.syndrome.pos=CD.syndrome[CD.syndrome$PÅVISAD=="Påvisad",]
-    
-    a.pos=raw_to_syndromicW(id=INSÄNTMATERIAL,
-                            syndromes.var=AGENS,
-                            syndromes.name=syndromes.name,
-                            dates.var=ANKOMSTDATUM,
-                            date.format = "%d/%m/%Y",
-                            min.date=new.data.start,
-                            max.date=new.data.end,
-                            data=CD.syndrome.pos)
-    
-    svaga.new.all <- rbindlist(list(as.data.frame(svaga.object[[sp.position]][[s]][,,1]),
-                                    as.data.frame(a.all@observed)),
-                               fill=TRUE)
-    svaga.new.pos <- rbindlist(list(as.data.frame(svaga.object[[sp.position]][,,2]),
-                                    as.data.frame(a.pos@observed)),
-                               fill=TRUE)
-    
-    
-    svaga.object[[sp.position]][[s]] <- abind(svaga.new.all,svaga.new.pos,along=3)
-    
-  }
-  
+#2018-08-14: ENV was not doing svaga "by syndromes", but for all ENV. The problem with that,
+#is the way the structures of data are expected, in particular to plot things in Shiny
+#revert back to syndrome specific summaries
+source("svaga-data-filtering.r",local=TRUE,encoding="native.enc")
+# 
+# 
+# #for (s in c(sp.colnames[[sp.position]],"Nonspecific")){
+#   #svaga for ENV NOT CONSIDERING SYNDROMES
+#   
+#   #CD.syndrome <- CD.species[CD.species$SYNDROMIC==s,c("AGENS","PÅVISAD","INSÄNTMATERIAL","ANKOMSTDATUM")]
+#   CD.syndrome <- CD.species[,c("AGENS","PÅVISAD","INSÄNTMATERIAL","ANKOMSTDATUM")]
+#   
+# #non.svaga.object[[sp.position]][[s]]<-CD.species[CD.species$AGENS=="",]
+#   
+#   
+#   CD.syndrome<-CD.syndrome[CD.syndrome$AGENS!="",]
+#   
+#   if(dim(CD.syndrome)[1]>0){
+#     
+#     svaga.object[[sp.position]][[s]] <- svaga.object[[sp.position]][[s]][1:last.historical.row.week,,,drop=FALSE]
+#     
+#     CD.syndrome$AGENS<- str_replace(CD.syndrome$AGENS, "\\(\\+\\)", "")
+#     syndromes.name=unique(c(colnames(svaga.object[[sp.position]][[s]]),CD.syndrome$AGENS))
+#     
+#     a.all=raw_to_syndromicW(id=INSÄNTMATERIAL,
+#                             syndromes.var=AGENS,
+#                             syndromes.name=syndromes.name,
+#                             dates.var=ANKOMSTDATUM,
+#                             date.format = "%d/%m/%Y",
+#                             min.date=new.data.start,
+#                             max.date=new.data.end,
+#                             data=CD.syndrome)
+#     
+#     CD.syndrome.pos=CD.syndrome[CD.syndrome$PÅVISAD=="Påvisad",]
+#     
+#     a.pos=raw_to_syndromicW(id=INSÄNTMATERIAL,
+#                             syndromes.var=AGENS,
+#                             syndromes.name=syndromes.name,
+#                             dates.var=ANKOMSTDATUM,
+#                             date.format = "%d/%m/%Y",
+#                             min.date=new.data.start,
+#                             max.date=new.data.end,
+#                             data=CD.syndrome.pos)
+#     
+#     svaga.new.all <- rbindlist(list(as.data.frame(svaga.object[[sp.position]][[s]][,,1]),
+#                                     as.data.frame(a.all@observed)),
+#                                fill=TRUE)
+#     svaga.new.pos <- rbindlist(list(as.data.frame(svaga.object[[sp.position]][,,2]),
+#                                     as.data.frame(a.pos@observed)),
+#                                fill=TRUE)
+#     
+#     
+#     svaga.object[[sp.position]][[s]] <- abind(svaga.new.all,svaga.new.pos,along=3)
+#     
+#   }
+#   
   
 #}
 
